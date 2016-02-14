7780fc8959e74ab7e7d58946f689bf7cfef59ac3
JC Bautista <JC Bautista>
2016-02-13 16:58:39 -0800

    Initial commit

---
+++ src/ph/edu/dlsu/Main.java
@@ -0,0 +1,25 @@
+package ph.edu.dlsu;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s1 = "Welcome to My Life!";
+        String s2 = new String("WELCOME to My Life!");
+        System.out.println(s2);
+
+        if (s1.equalsIgnoreCase(s2)) {
+            System.out.println("They Match!");
+        }
+        else {
+            System.out.println("They don't Match!");
+        }
+        
+        char[] chars = s1.toCharArray();
+        for (char c : chars) {
+            System.out.println(c);
+
+        }
+            
+        }
+    }
